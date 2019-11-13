63069602a6c8e8f0d0a51d2d2eb413ed8d4f665a
Trey Morris <trey@morris2morris.com>
2019-11-12 21:37:18 -0500

    Update 11/12/19 21:36
    Added booleans and attempted to clean up the syntax and write cleaner code. Booleans help with the output of the FizzBuzz and overall make the code look cleaner.

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,26 +1,33 @@
 
 /**
- * Write a description of class Fizzbuzz here.
+ * 
  *
  * @author Trey Morris
  * 
  */
 public class Fizzbuzz
-{
-public static void main(String[]args){
- for(int i = 1; i<=100; i++)
-    {
-       if (i % 3 == 0) System.out.print("Fizz");
-       { if(i%5==0)System.out.print("Buzz");
+{ 
+public static void main(String[]args){ 
+    { 
+        int n = 100;
+   for(int i = 2; i<=n; i++){
+  
+   boolean DivisiblebyThree = i % 3==0;
+    boolean DivisiblebyFive = i % 5 == 0;
+       
+       if 
+       (DivisiblebyThree) System.out.print("Fizz");
+        if(DivisiblebyFive)System.out.print("Buzz");
           
-        else if (i % 3 != 0)System.out.print(i);System.out.println();
-         
-        }
-    }
-        }
-    }
-    
+        else if (i % 3 != 0)System.out.print(i);
+        System.out.println();
+}
+}
+}
+}
 
+ 
+   
         
 
 
